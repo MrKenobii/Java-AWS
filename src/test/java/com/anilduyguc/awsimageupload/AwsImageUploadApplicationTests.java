2810bb99b464d8a5965b0239e3d096e55d1eb73a
MrKenobii <anldygc35@windowslive.com>
2022-03-21 00:30:47 +0300

    Initial commit

---
+++ src/test/java/com/anilduyguc/awsimageupload/AwsImageUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anilduyguc.awsimageupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsImageUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
